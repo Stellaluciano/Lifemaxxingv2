01d876432531e5083cc5708b2a025e4f41191748
Douglas Jiang <douglasjiang@JYDOUGLAS-MacBook-Pro.local>
2025-10-31 03:48:50 -0400

    firebase.js saved so keys don't show

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBjtV9YPp2djn3xDUCz_kb-oMKSLU6UXn0",
-  authDomain: "lifemaxxing-4b353.firebaseapp.com",
-  projectId: "lifemaxxing-4b353",
-  storageBucket: "lifemaxxing-4b353.firebasestorage.app",
-  messagingSenderId: "471719994299",
-  appId: "1:471719994299:web:ea01493f86b83e29df5a9e",
-  measurementId: "G-45R5TQQGVF"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
