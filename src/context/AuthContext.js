ce3b085355a744ebeb801b59bafcb24dfaedb7a1
Stellaluciano <ftianmcb@gmail.com>
2026-01-29 20:45:19 -0500

    Update AuthContext.js

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -2,7 +2,10 @@ import { createContext, useContext, useEffect, useState } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../firebase";
 
-const AuthContext = createContext(null);
+const AuthContext = createContext({
+  user: null,
+  loading: true,
+});
 
 export function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
